976286f761f6bb59cb3009f03b153d4e00ce804f
Adam Gajek <adam.gajek777@gmail.com>
2021-03-05 13:51:13 +0100

    bikemicroservice/feature/add-domain-entities

---
+++ BackEnd/BikeMicroservice/src/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class BaseEntity
+    {
+        public string Id { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+    }
+}
---
+++ BackEnd/BikeMicroservice/src/Domain/Entities/Model.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class Model : BaseEntity
+    {
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        [DefaultValue(Type.City)]
+        public Type Type { get; set; }
+
+        [Required]
+        public bool Shocks { get; set; }
+
+        [Required]
+        [Range(16, 31)]
+        public int Gears { get; set; }
+
+        [Required]
+        [Range(16.0, 30.0)]
+        public double WheelSize { get; set; }
+    }
+}
