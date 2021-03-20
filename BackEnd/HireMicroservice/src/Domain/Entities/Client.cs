04b6ec9adc6d41d75ebee9e00d60db967022f859
Adam Gajek <adam.gajek777@gmail.com>
2021-03-20 15:47:31 +0100

    feature/hiremicroserv-add-entities

---
+++ BackEnd/HireMicroservice/src/Domain/Entities/Client.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class Client : BaseEntity
+    {
+        [Range(0,5)]
+        public int NumOfHires { get; set; }
+
+        public virtual ICollection<Hire> Hires { get; set; }
+    }
+}
---
+++ BackEnd/HireMicroservice/src/Domain/Entities/Hire.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    public class Hire : BaseEntity
+    {
+        [Required]
+        public string ClientId { get; set; }
+        [ForeignKey(nameof(ClientId))]
+        public virtual Client Client { get; set; }
+        
+        [Required] 
+        public string BikeId { get; set; }
+        [ForeignKey(nameof(BikeId))]
+        public virtual Bike Bike { get; set; }
+    }
+}
