e58249ee9f87f950aabb5786e5d5e90bb2e09f0e
Adam Gajek <adam.gajek777@gmail.com>
2021-03-20 21:05:37 +0100

    add hireservice implementation

---
+++ BackEnd/HireMicroservice/src/Application/Common/Exceptions/InternalServerException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Application.Common.Exceptions
+{
+    public class InternalServerException : Exception
+    {
+        public InternalServerException(string message)
+            : base(message)
+        {
+        }
+    }
+}
