8cd17eaa2254bc153e07d11cd933d8ca851a7946
Adam Gajek <adam.gajek777@gmail.com>
2021-04-11 17:46:16 +0200

    feature/apigateway-add-default-routes

--- BackEnd/ApiGateway/src/WebUI/Startup.cs
+++ BackEnd/ApiGateway/src/WebUI/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
@@ -33,22 +34,31 @@ namespace WebUI
             services.AddControllers();
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "WebUI", Version = "v1"}); });
 
-            var authenticationProviderKey = Encoding.UTF8.GetBytes(Configuration.GetSection("Jwt:Type").ToString() ?? string.Empty);
+            var authenticationProviderKey = Encoding.UTF8.GetBytes("This is my secret key");
 
             services.AddAuthentication(options =>
                 {
                     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
                 })
-                .AddJwtBearer(authenticationProviderKey.ToString(), options =>
+                .AddJwtBearer(options =>
                 {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        IssuerSigningKey = new SymmetricSecurityKey(authenticationProviderKey),
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
                 });
             
-            services.AddOcelot(Configuration);
+            services.AddOcelot();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async Task Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -57,7 +67,7 @@ namespace WebUI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebUI v1"));
             }
 
-            await app.UseOcelot();
+            app.UseAuthentication();
 
             app.UseHttpsRedirection();
 
@@ -66,6 +76,8 @@ namespace WebUI
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            
+            app.UseOcelot().Wait();
         }
     }
 }
