1e569411088973e8e2442523344191e5b022c752
Adam Gajek <adam.gajek777@gmail.com>
2021-04-11 17:44:51 +0200

    add single aggregation

---
+++ BackEnd/BikeMicroservice/src/Application/Bikes/Handlers/GetBikesByHomeBaseIdHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Bikes.Queries;
+using Domain.DTOs;
+using LanguageExt.Common;
+using MediatR;
+
+namespace Application.Bikes.Handlers
+{
+    public class GetBikesByHomeBaseIdHandler : IRequestHandler<GetAllBikesQuery, Result<List<BikeResponse>>>
+    {
+        public Task<Result<List<BikeResponse>>> Handle(GetAllBikesQuery request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BackEnd/BikeMicroservice/src/Application/Bikes/Queries/GetBikesByHomeBaseId.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Domain.DTOs;
+using LanguageExt;
+using LanguageExt.Common;
+using MediatR;
+
+namespace Application.Bikes.Queries
+{
+    public class GetBikesByHomeBaseId : IRequest<Result<List<BikeResponse>>>
+    {
+        public string HomeBaseId { get; set; }
+
+        public GetBikesByHomeBaseId(string homeBaseId)
+        {
+            HomeBaseId = homeBaseId;
+        }
+    }
+}
